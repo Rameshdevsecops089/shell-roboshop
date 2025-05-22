f2fca90ef6f66c0de939ff1c0f62f27387fd906d
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-22 14:17:06 +0530

    roboshop data

--- roboshop.sh
+++ roboshop.sh
@@ -11,7 +11,7 @@ for instance in ${INSTANCES[@]}
 do 
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-098c3eca35649f2c5 --tag-specifications "ResourceType=instance,
     Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].PrivateIpAddress" --output text)
-    if [ $Instance != "frontend" ]
+    if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].
         Instances[0].PrivateIpAddress" --output text)
