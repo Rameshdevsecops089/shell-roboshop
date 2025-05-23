d1a9816a2f53c7bc0f9ba94f3227065166f70c39
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-23 15:08:57 +0530

    roboshop data

--- roboshop.sh
+++ roboshop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-AMI_ID="ami-09c813fb71547fc4f"
+AMI_ID="ami-09c813fb71547fc4f" 
 SG_ID="sg-098c3eca35649f2c5" # replace with your SG_ID
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalaogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+INSTANCES=("mongodb" "redis" "mysql")
 ZONE_ID="Z0509728XFPLN8X44IK2" # replace with your ZONE ID
-DOMAIN_NAME="daws84.space" # replace with your domain
+DOMAIN_NAME="daws84.space" # replace with your DOMAIN Name
 
 for instance in ${INSTANCES[@]}
 do 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_I --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].PrivateIpAddress" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-098c3eca35649f2c5 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].PrivateIpAddress" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0]. Instances[0].PrivateIpAddress" --output text)
@@ -16,22 +16,4 @@ do
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0]. Instances[0].PublicIpAddress" --output text)
     fi
     echo "$instance IP address: $IP"
-
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Creating or Updating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$instance'.'$DOMAIN_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
-            }]
-        }
-        }]
-    }'
 done
