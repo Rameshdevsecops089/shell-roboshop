b18a91329e799485264644eec1568eb463fc73c7
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-26 16:57:06 +0530

    create record

--- roboshop.sh
+++ roboshop.sh
@@ -6,14 +6,16 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z0509728XFPLN8X44IK2" # replace with your ZONE ID
 DOMAIN_NAME="daws84.space" # replace with your DOMAIN Name
 
-for instance in ${INSTANCES[@]}
+for instance in $@
 do  
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-098c3eca35649f2c5 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        Record_Name="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        Record_Name="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
 
@@ -25,7 +27,7 @@ do
         ,"Changes": [{
         "Action"              : "CREATE"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$instance'.'$DOMAIN_NAME'"
+            "Name"              : "'$DOMAIN_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
