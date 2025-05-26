8f07ebfeda8431ae55456f9d7d4d78ca5219fcb9
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-26 18:29:19 +0530

    create record

--- redis.sh
+++ redis.sh
@@ -42,3 +42,16 @@ VALIDATE $? "Enabling Redis:7"
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing Redis"
 
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Edited redis.conf to accept remote connections"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis"
+
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "started Redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "script execution completed successfully, $Y time taken: $TOTAL_TIME $N | tee -a $LOG_FILE"
