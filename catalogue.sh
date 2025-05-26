d99c194ad3738fc3ff7abc2080155f15c0fdf8ec
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-26 13:49:36 +0530

    create record

---
+++ catalogue.sh
@@ -0,0 +1,73 @@
+#!bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root privileges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error:: please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "you are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate function takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$2 is ... $G success $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R Failure $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling nodejs:20"
+
+dnf install nodejs -y $LOG_FILE
+VALIDATE $? "installing nodeja:20"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "Creating roboshop system user"
+
+mkdir /app 
+VALIDATE $? "Creting app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? "Downloading catalogue"
+
+cd /app 
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzipping catalogue"
+
+npm install $LOG_FILE
+VALIDATE $? "Installing Dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copying catalogue service"
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable catalogue 
+systemctl start catalogue
+VALIDATE $? "Starting Catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y
+VALIDATE $? "installing MongoDB Client"
+
+mongosh --host mongod.daws84.space </app/db/master-data.js &>>$LOG_FILE
