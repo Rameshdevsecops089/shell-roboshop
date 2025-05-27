53742c4acc47cfddb20b8d4d7effa3e37f5a9d9e
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-27 17:39:36 +0530

    added mysql

---
+++ mysql.sh
@@ -0,0 +1,55 @@
+#!bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root privileges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error:: please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "you are running with root access" | tee -a $LOG_FILE
+fi
+
+echo "please enter root password to setup"
+read -s MYSQL_ROOT_PASSWORD
+
+# validate function takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$2 is ... $G success $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R Failure $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabling MySQL"
+
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "Starting MySQL"
+
+mysql_secure_installation --set-root-pass read -s MYSQL_ROOT_PASSWORD &>>$LOG_FILE
+VALIDATE $? "Setting MySQL root password"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N | tee -a $LOG_FILE"
