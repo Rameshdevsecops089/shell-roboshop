7c7a792ddbe477ece2496e2e95256c9c2a84432a
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-27 18:22:24 +0530

    added mysql

--- shipping.sh
+++ shipping.sh
@@ -80,9 +80,9 @@ VALIDATE $? "starting shipping"
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "install mysql"
 
-mysql -h mysql.daws84.space -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-mysql -h mysql.daws84.space -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
-mysql -h mysql.daws84.space -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+mysql -h mysql.daws84.space -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+mysql -h mysql.daws84.space -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql &>>$LOG_FILE
+mysql -h mysql.daws84.space -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
 VALIDATE $? "Loading data into my sql"
 
 systemctl restart shipping &>>$LOG_FILE
