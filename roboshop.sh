ac5839ac5d9d1aee0dd6dbb83bce20be8cdd562a
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-11-17 22:50:00 +0530

    instance create

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,7 @@ DOMAIN_NAME="daws84.space" # replace with your DOMAIN Name
 
 #for instance in ${INSTANCES[@]}
 
-for instance in $@
+for instance in ${INSTANCES[@]}
 do  
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-051f07331519e47f6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
@@ -20,5 +20,4 @@ do
      #   RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
-
 done
