82ca821042395003970df1b009337e06e3693217
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-11-17 22:36:26 +0530

    instance create

--- roboshop.sh
+++ roboshop.sh
@@ -7,19 +7,20 @@ ZONE_ID="Z040420130TZZP02OZ4VV" # replace with your ZONE ID
 DOMAIN_NAME="daws84.space" # replace with your DOMAIN Name
 
 #for instance in ${INSTANCES[@]}
+
 for instance in $@
 do  
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-098c3eca35649f2c5 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-051f07331519e47f6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME"
+     #   RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        RECORD_NAME="$DOMAIN_NAME"
+     #   RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
-
+ : '
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
@@ -37,4 +38,5 @@ do
         }
         }]
     }'
+'
 done
