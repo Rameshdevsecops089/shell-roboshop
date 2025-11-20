0157da8ca3ded410d28b06d9b23b41c813a0e19e
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-11-20 23:15:44 +0530

    while-loop

---
+++ user-set.sh
@@ -0,0 +1,54 @@
+#!bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root privileges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error:: please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "you are running with root access" | tee -a $LOG_FILE
+fi
+
+dnf module disable nodejs -y &>>$LOG_FILE
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+dnf install nodejs -y &>>$LOG_FILE
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+else
+    echo -e "system user roboshop already creted ... $Y skipping $N"
+fi
+
+mkdir -p /app 
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+
+rm -rf /app/*
+cd /app 
+unzip /tmp/user.zip &>>$LOG_FILE
+npm install &>>$LOG_FILE
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable user &>>$LOG_FILE
+systemctl start user &>>$LOG_FILE
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N | tee -a $LOG_FILE"
+
---
+++ while-loop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+a=0
+
+while [ $a -it 10]
+do
+    echo $a
+    a= 'expr $a + 1'
+done
